55a874c891300ae76067a51fb41abbde95d1c4e2
lawrence-jang <jkb2221@gmail.com>
2025-08-02 02:27:12 +0900

    🧹 chore: add svg files

---
+++ public/icons/RetryIcon.tsx
@@ -0,0 +1,12 @@
+export const RetryIcon = (props: React.SVGAttributes<SVGElement>) => (
+  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 21 21' {...props}>
+    <path
+      fill='none'
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      d='M10 8H5V3m.291 13.357a8 8 0 1 0 .188-8.991'
+    />
+  </svg>
+)
